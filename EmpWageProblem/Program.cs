ee87d16d1cbdebf4a4349061b6b3f79d9cbe3781
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-01-03 22:24:17 +0530

    [Sartaj] : added Employee Wage Calculate

--- EmpWageProblem/Program.cs
+++ EmpWageProblem/Program.cs
@@ -1,3 +1,5 @@
 ﻿using EmpWageProblem;
 WageComputation calculation = new WageComputation();
 calculation.EmployeeAttendence();
+WageComputation calculation2 = new WageComputation();
+calculation2.EmpWageCalculation();
