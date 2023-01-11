add03c29b0dee688bbe9a439c165e8c0532f8a95
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-01-11 22:35:49 +0530

    [Sartaj] : added managing Employee wage for multiple company program

--- EmpWageProblem/Program.cs
+++ EmpWageProblem/Program.cs
@@ -1,9 +1,10 @@
 ﻿using EmpWageProblem;
 Console.WriteLine("Welcome To The Employee Wage Calculation Program");
-WageComputation attendancecheck = new WageComputation();
-attendancecheck.EmployeeAttendence();
-WageComputation empwagecomputation = new WageComputation();
+//WageComputation attendancecheck = new WageComputation();
+//attendancecheck.EmployeeAttendence();
+WageComputation empwagecomputation = new WageComputation(2);
 empwagecomputation.AddCompany("Reliance", 20, 8, 4, 100, 20);
-empwagecomputation.AddCompany("Tata", 20, 8, 4, 100, 20);
 empwagecomputation.EmpWageCalculation("reliance");
+empwagecomputation.AddCompany("Tata", 25, 9, 5, 90, 24);
 empwagecomputation.EmpWageCalculation("tata");
+empwagecomputation.DisplayWage();
