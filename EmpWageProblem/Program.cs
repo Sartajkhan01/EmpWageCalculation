26c8bf2fa98ecb664a5f51168cd9dd718e947695
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-01-13 22:37:33 +0530

    [Sartaj] : added for multiple company storing daily wage with total wage

--- EmpWageProblem/Program.cs
+++ EmpWageProblem/Program.cs
@@ -1,10 +1,10 @@
 ﻿using EmpWageProblem;
 Console.WriteLine("Welcome To The Employee Wage Calculation Program");
-//WageComputation attendancecheck = new WageComputation();
-//attendancecheck.EmployeeAttendence();
-WageComputation empwagecomputation = new WageComputation(2);
-empwagecomputation.AddCompany("Reliance", 20, 8, 4, 100, 20);
-empwagecomputation.EmpWageCalculation("reliance");
-empwagecomputation.AddCompany("Tata", 25, 9, 5, 90, 24);
-empwagecomputation.EmpWageCalculation("tata");
-empwagecomputation.DisplayWage();
+EmployeeWageComputation employeeComputation = new EmployeeWageComputation(2);
+employeeComputation.AddCompany("TATA", 20, 8, 4, 100, 20);
+employeeComputation.CalculateEmpWage("TATA");
+
+employeeComputation.AddCompany("MAHINDRA", 30, 8, 4, 100, 20);
+employeeComputation.CalculateEmpWage("MAHINDRA");
+
+employeeComputation.displayArray();
