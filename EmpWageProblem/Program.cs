75192a7daeaadc8cbe15531678218de4b8bf1207
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-01-13 22:41:46 +0530

    [Sartaj] : added total wage quaried by the company

--- EmpWageProblem/Program.cs
+++ EmpWageProblem/Program.cs
@@ -1,10 +1,17 @@
 ﻿using EmpWageProblem;
+
 Console.WriteLine("Welcome To The Employee Wage Calculation Program");
-EmployeeWageComputation employeeComputation = new EmployeeWageComputation(2);
-employeeComputation.AddCompany("TATA", 20, 8, 4, 100, 20);
-employeeComputation.CalculateEmpWage("TATA");
+//WageComputation attendancecheck = new WageComputation();
+//attendancecheck.EmployeeAttendence();
+WageComputation empwagecomputation = new WageComputation(3);
+empwagecomputation.AddCompany("Reliance", 20, 8, 4, 100, 20);
+empwagecomputation.EmpWageCalculation("reliance");
+empwagecomputation.AddCompany("Tata", 25, 9, 5, 90, 24);
+empwagecomputation.EmpWageCalculation("tata");
+empwagecomputation.AddCompany("Birla", 22, 7, 6, 110, 25);
+empwagecomputation.EmpWageCalculation("birla");
 
-employeeComputation.AddCompany("MAHINDRA", 30, 8, 4, 100, 20);
-employeeComputation.CalculateEmpWage("MAHINDRA");
 
-employeeComputation.displayArray();
+Console.Write("Enter the name of the company you want to check the total wage : ");
+string name = Console.ReadLine();
+empwagecomputation.DisplayWage(name);
